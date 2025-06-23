6c1f667de185aea7a06d1b020922e0be12604ff1
Vaibhav Sharma <v-sharmavaib@microsoft.com>
2025-06-23 10:48:26 +0530

    Shorten Fibonacci implementation

--- Fibonacci.py
+++ Fibonacci.py
@@ -1,16 +1,7 @@
-# Function to generate Fibonacci series up to n terms
-def fibonacci_series(n):
-    fib_series = [0, 1]
-    while len(fib_series) < n:
-        next_term = fib_series[-1] + fib_series[-2]
-        fib_series.append(next_term)
-    return fib_series
+def fibonacci(n):
+    a, b = 0, 1
+    for _ in range(n):
+        print(a)
+        a, b = b, a + b
 
-# Number of terms
-n_terms = 20
-
-# Generate and display the Fibonacci series
-fib_series = fibonacci_series(n_terms)
-print("Fibonacci series up to", n_terms, "terms:")
-for term in fib_series:
-    print(term)
+fibonacci(20)
